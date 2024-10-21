fb2829e9690947f37587bdba68f4708d56b1dce2
Allan Liu <allanliu236@gmail.com>
2024-10-21 14:33:32 -0400

    look at freeElectronSim_11th_Oct 2.py, trying to figure out why avalanche ionization is so weak, i think it has to do with the units for laser intensity

--- freeElectronSim_11th_Oct 2.py
+++ freeElectronSim_11th_Oct 2.py
@@ -4,7 +4,7 @@ from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
 
 # Constants
-FWHM = 175  # FWHM in fs
+FWHM = 350e-15  # FWHM in fs
 I_peak = 100  # Peak intensity
 P_constant = 9.52e10  # Constant in P(I)
 sigma = FWHM / 2.355  # Convert FWHM to sigma for Gaussian
@@ -15,31 +15,36 @@ time_ps = time_fs / 1000  # Convert time to picoseconds for rate equation
 
 # Define the Gaussian function for I(t)
 def I_t(t, I_peak, sigma):
-    return I_peak * np.exp(-((t - 175) ** 2) / (2 * sigma ** 2))  # Center at 175 fs
+    return I_peak * np.exp(-((t - 175e-15) ** 2) / (2 * sigma ** 2))  # Center at 175 fs
 
 # Define P(I) as a function of time
 def P_I(t):
     I = I_t(t, I_peak, sigma)
-    return P_constant * I**8
+    return P_constant * I**8 * 1e3
+    
+def avalanche(t):
+    I = I_t(t, I_peak, sigma)
+    return I
 
-# Rate equation: dn/dt = P(I) * n + P(I)
-def rate_equation(t, n):
-    return  P_I(t)
+def rate_equation(n, t):
+    return 4*avalanche(t)*n 
 
-# Initial condition for n(t) - Try with a small non-zero value
-n0 = [0]  # Starting with a small initial condition for n(t)
+t = np.linspace(0,350,1000)*1e-15
 
-
-
-# Solve the differential equation using solve_ivp
-n_t = odeint(rate_equation, n0, time_fs)
-
-# Plot the solution
-plt.figure(figsize=(8, 6))
-plt.plot(time_fs, n_t, label='n(t)', color='blue')
-plt.xlabel('Time (fs)')
-plt.ylabel('n(t)')
-plt.title('Evolution of n(t) over 350 fs')
-plt.grid(True)
-plt.legend()
+n_t = odeint(rate_equation, 1, t)
+plt.plot(t*1e15,n_t)
 plt.show()
+
+
+# # Solve the differential equation using solve_ivp
+# n_t = odeint(P_I, n0, time_fs)
+
+# # Plot the solution
+# plt.figure(figsize=(8, 6))
+# plt.plot(time_fs, n_t, label='n(t)', color='blue')
+# plt.xlabel('Time (fs)')
+# plt.ylabel('n(t)')
+# plt.title('Evolution of n(t) over 350 fs')
+# plt.grid(True)
+# plt.legend()
+# plt.show()
