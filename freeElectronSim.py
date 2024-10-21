ad88065c32ab16d8116d3e85383162ec41581629
Allan Liu <allanliu236@gmail.com>
2024-10-21 01:05:22 -0400

    just need to figure out the avalanche part

--- freeElectronSim.py
+++ freeElectronSim.py
@@ -15,27 +15,23 @@ bohr_radius = 5.29e-11
 e_osc_eV = 19 #according to Jingsen, but I calculate 9.866eV
 laser_angular_freq = 1.83e15
 
-def cw(current_electron_density):
-    return 0.31*math.pi*bohr_radius*(current_electron_density**(1./3))
+FWHM = 175  # FWHM in fs
+I_peak = 100  # Peak intensity
+P_constant = 9.52e10  # Constant in P(I)
+sigma = FWHM / 2.355  # Convert FWHM to sigma for Gaussian
 
-def p_exchange(current_electron_density):
-    return (3*cw(current_electron_density)*boltzmann_constant_J*87000)/(2*reduced_planck_J) # temp temperature swap from 273.15 to 87000
+# Define the Gaussian function for I(t)
+def I_t(t, I_peak, sigma):
+    return I_peak * np.exp(-((t - 175) ** 2) / (2 * sigma ** 2))  # Center at 175 fs
 
-def w_imp(current_electron_density):
-    eV_term = e_osc_eV/9.0 #9.0eV is the band gap of SiO2
-    rate_term_numerator = ((laser_angular_freq)**2)*p_exchange(current_electron_density)
-    rate_term_denominator = (p_exchange(current_electron_density)**2) + (laser_angular_freq**2)
-    rate_term = rate_term_numerator/rate_term_denominator
-    return eV_term*rate_term
+def P_I(t):
+    I = I_t(t, I_peak, sigma)
+    return P_constant * I**8 *1e12
 
 #Scipy approach - seems to be working
-current_free_atom_count = 6.6e22
+
 def dndt(t, ne):
-    na = current_free_atom_count - ne
-    if (na <= 0):
-        na = 0
-    
-    return na*w_mpi
+    return 4*P_I(t)*ne + P_I(t) - ne
 ne0 = 0
 
 t = np.linspace(0, 300*pow(10,-15), 30000)
--- freeElectronSim_11th_Oct 2.py
+++ freeElectronSim_11th_Oct 2.py
@@ -1,51 +1,45 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import math
-import numpy as np 
-from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
-w_mpi = 7.292721e14 #7.809699e14 assuming nph = 5.8, but photons are discrete so i use 6
+from scipy.integrate import odeint
 
-electron_mass = 9.1093837e-31 #kg
-electron_charge = 1.60217663e-19 #C
-reduced_planck_eV = 6.582119569e-16 #eV
-reduced_planck_J = 1.054571814e-34 #J
-boltzmann_constant_eV = 8.617333262e-5 #eV
-boltzmann_constant_J = 1.380649e-23 #J
-bohr_radius = (reduced_planck_J**2)/(electron_mass*(electron_charge**2))
-e_osc_eV = 20 #9.86637 #eV or 1.8659e15 depending on equation used
-laser_angular_freq = 1.83e15
+# Constants
+FWHM = 175  # FWHM in fs
+I_peak = 100  # Peak intensity
+P_constant = 9.52e10  # Constant in P(I)
+sigma = FWHM / 2.355  # Convert FWHM to sigma for Gaussian
+
+# Time array in femtoseconds
+time_fs = np.linspace(0, 350, 1000) 
+time_ps = time_fs / 1000  # Convert time to picoseconds for rate equation
+
+# Define the Gaussian function for I(t)
+def I_t(t, I_peak, sigma):
+    return I_peak * np.exp(-((t - 175) ** 2) / (2 * sigma ** 2))  # Center at 175 fs
+
+# Define P(I) as a function of time
+def P_I(t):
+    I = I_t(t, I_peak, sigma)
+    return P_constant * I**8
+
+# Rate equation: dn/dt = P(I) * n + P(I)
+def rate_equation(t, n):
+    return  P_I(t)
+
+# Initial condition for n(t) - Try with a small non-zero value
+n0 = [0]  # Starting with a small initial condition for n(t)
 
 
 
+# Solve the differential equation using solve_ivp
+n_t = odeint(rate_equation, n0, time_fs)
 
-# Gaussian function for intensity
-def gaussian_intensity(t):
-    FWHM = 350e-15  # FWHM in seconds (350 fs)
-    I_0 = 1e14      # Peak intensity in W/cm^2
-    t_0 = 175e-15         # Center of the Gaussian pulse, at t = 0
-    return (I_0 * np.exp(-4 * np.log(2) * (t - t_0)**2 / FWHM**2))/1e12 # /1e12 to get TW/cm^2
-
-def multiphoton(t):
-    return (9.523e10 * (gaussian_intensity(t)**8))*1e12
-
-rho = []
-t = np.linspace(0,350,3500)*1e-15
-ne = 0
-for dt in t:
-    print(ne/150e-15)
-    ne = multiphoton(dt) + 4*multiphoton(dt)*ne - (ne/(1e-15))
-    rho.append(ne)
-
-# def dndt(t, ne):
-#     tau = 1e-15
-
-#     return 4*multiphoton(t)*ne + multiphoton(t) - (ne/tau)
-# ne0 = 0
-# t = np.linspace(0, 350*pow(10,-15), 30000)
-# sol = solve_ivp(dndt, t_span=(0, max(t)), y0=[ne0], t_eval=t, method="Radau")
-# rho = sol.y[0]
-
-plt.plot(t*1e15, rho)
+# Plot the solution
+plt.figure(figsize=(8, 6))
+plt.plot(time_fs, n_t, label='n(t)', color='blue')
+plt.xlabel('Time (fs)')
+plt.ylabel('n(t)')
+plt.title('Evolution of n(t) over 350 fs')
 plt.grid(True)
+plt.legend()
 plt.show()
-plt.close()
